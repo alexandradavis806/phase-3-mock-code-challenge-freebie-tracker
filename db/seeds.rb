8292f5671ee8c1f32b62fa31f7249ce11601f915
Alexandra Davis <alex.davis806@gmail.com>
2022-05-02 16:05:09 -0700

    commit tree 08403c2c4be5f2dba53a80815baa6877e907d918

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,21 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder_mifflin = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+meseeks = Dev.create(name: "Mr. Meseeks")
+gazorpazop = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+
+Freebie.create(item_name: 'pen', value: 3, company_id: google.id, dev_id: rick.id)
+Freebie.create(item_name: 'mousepad', value: 5, company_id: google.id, dev_id: morty.id)
+Freebie.create(item_name: 'charger', value: 15, company_id: facebook.id, dev_id: meseeks.id)
+Freebie.create(item_name: 'water bottle', value: 8, company_id: enron.id, dev_id: rick.id)
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
